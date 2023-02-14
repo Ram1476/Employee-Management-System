999cad5ee2c566b15b5dc1e164fbc14c8f756021
Ram1476 <ramilangovan.1299@gmail.com>
2023-02-14 23:33:37 +0530

    Add project files.

---
+++ EmployeeManagementSystem/Employee_System.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpSystem
+{
+    internal class Employee_System
+    {
+        public int employee_Id { get; set; }
+        public string employee_Name { get; set; }
+
+        public DateTime employee_DOB { get; set; }
+
+        public double employee_Salary { get; set; }
+
+        public bool employee_Active_Status { get; set; }
+
+        //ArrayList Employee_Details = new ArrayList();
+
+        public Employee_System Create_Employee()
+        {
+            Console.Write("Enter The Number of Employee Details You Want to Enter: ");
+            int Count = int.Parse(Console.ReadLine());
+            Employee_System employee = new Employee_System();
+
+            int i = 0;
+            while (i < Count)
+            {
+
+                Console.Write("\nEnter the Employee_ID: ");
+                employee.employee_Id = int.Parse(Console.ReadLine());
+
+                Console.Write("\nEnter Employee_Name: ");
+                employee.employee_Name = Console.ReadLine();
+
+                Console.Write("\nEnter Employee DateOfBirth: ");
+                employee.employee_DOB = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("\nEnter Employee_Salary: ");
+                employee.employee_Salary = double.Parse(Console.ReadLine());
+
+                Console.Write("\nEnter Employee_Active Status: ");
+                string user_Input = Console.ReadLine();
+                if (user_Input != null && user_Input.ToUpper() == "Y")
+                {
+                    employee.employee_Active_Status = true;
+                }
+                else
+                {
+                    employee.employee_Active_Status = false;
+                }
+                Console.WriteLine("\nYou Employee data Has been Successfully Created");
+
+                i++;
+            }
+
+
+            return employee;
+
+        }
+
+        public void Update_Employee(ref ArrayList employeeList)
+        {
+            bool isDataupdated = false;
+            Employee_System employee = new Employee_System();
+            Console.Write("\nEnter the Employee_ID Your Want to Update: ");
+            employee.employee_Id = int.Parse(Console.ReadLine());
+            Console.Write("\nEnter the Salary Value Need to be Updated: ");
+            employee.employee_Salary = double.Parse(Console.ReadLine());
+            foreach (Employee_System emp in employeeList)
+            {
+                if (emp.employee_Id == employee.employee_Id)
+                {
+                    emp.employee_Salary = employee.employee_Salary;
+                    isDataupdated = true;
+                    break;
+                }
+            }
+            if (isDataupdated)
+            {
+                Console.WriteLine("\n******************");
+                Console.WriteLine("\n Your Preferred Employee Details Is Been updated\nThank You using the Employee Management System");
+                Console.WriteLine("\n******************");
+            }
+            else
+            {
+                Console.WriteLine("Enter a Valid Employee detail");
+            }
+        }
+
+        public void view_Employee(ref ArrayList employeeList)
+        {
+
+        }
+
+
+    }
+}
---
+++ EmployeeManagementSystem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee_System user1 = new Employee_System();
+            string user_Choice = "";
+            ArrayList employee = new ArrayList();
+            Console.WriteLine("****** Welcome to the Employee Management System ******");
+            Console.WriteLine("\n******************************");
+
+            Console.WriteLine("\n1.To Create New Employee Data\n2.To Update Employee Data\n3. Delete Employee data\n4.View All Employee data\n5.View Employee data with selete ID");
+            Console.WriteLine("\n******************************");
+
+            Console.Write("\nEnter the Number Corresponding to the function you Want to execute:");
+
+
+
+            while (user_Choice.ToUpper() != "QUIT")
+            {
+                user_Choice = Console.ReadLine();
+
+                switch (user_Choice)
+                {
+                    case "1":
+                        employee.Add(user1.Create_Employee());
+                        break;
+                    case "2":
+                        user1.Update_Employee(ref employee);
+                        break;
+                    default:
+                        Console.WriteLine("Enter a Valid Input To Continue:");
+                        break;
+                        //case "3":
+                        //    user
+
+                }
+                Console.Write("\nEnter the Value for the next function if u want to continue or Enter \'Quit\' to End this Session: ");
+                user_Choice = Console.ReadLine();
+            }
+            Console.WriteLine("\n****** Thank You For Using the Employee Management System ****** ");
+            Console.ReadLine();
+
+        }
+    }
+}
